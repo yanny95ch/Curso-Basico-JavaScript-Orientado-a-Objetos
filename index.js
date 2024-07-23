629d7cce865f0d3f07700edfb70f178588d68215
yannych1995 <yannymaly@gmal.com>
2024-07-23 18:00:02 -0500

    Que es un Objeto

---
+++ index.js
@@ -0,0 +1,8 @@
+function Student(){
+    this.name = 'Nombre';
+    this.age = 18;
+    this.point = 760;
+}
+const juanita = new Student();
+
+console.log(juanita);// Student { name: 'Nombre', age: 18, point: 760 } : 'Student' nos indica que el objeto juanita es una instancia frl prototipo Student
